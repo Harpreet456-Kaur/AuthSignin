417dd19bf5190c9c1bc7c9a56738b0f3dd445932
Harpreet456-Kaur <125293917+Harpreet456-Kaur@users.noreply.github.com>
2023-03-13 15:57:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/authsignin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.authsignin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.authsignin.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        auth = Firebase.auth
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            if (binding.etEmail.text.toString().isEmpty()){
+                binding.etEmail.error="Enter your email"
+            }
+            else if (binding.etPass.text.toString().isEmpty()) {
+                binding.etPass.error = "Enter your email"
+            }
+            else{
+                auth.createUserWithEmailAndPassword(
+                    binding.etEmail.text.toString(), binding.etPass.text.toString()).addOnSuccessListener {
+                    Toast.makeText(applicationContext, "Successfully Login", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, FirebaseAuthException::class.java))
+                    finish()
+                }
+                    .addOnFailureListener {
+                        Toast.makeText(applicationContext, "Error${it.toString()}", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+        binding.noAccount.setOnClickListener {
+                var intent=Intent(this,Register::class.java)
+                startActivity(intent)
+            }
+        }
+    }
---
+++ app/src/main/java/com/example/authsignin/Register.kt
@@ -0,0 +1,60 @@
+package com.example.authsignin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.authsignin.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class Register : AppCompatActivity() {
+    lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        auth = Firebase.auth
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            if (binding.etEmail.text.toString().isEmpty()) {
+                binding.etEmail.error = "Enter your email"
+            } else if (binding.etPassword.text.toString().isEmpty()) {
+                binding.etPassword.error = "Enter your email"
+            } else {
+                Toast.makeText(applicationContext, "Successfully Register", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.alreadyRegister.setOnClickListener {
+            if (binding.etEmail.text.toString().isEmpty()) {
+                binding.etEmail.error = "Enter your email"
+            } else if (binding.etPassword.text.toString().isEmpty()) {
+                binding.etPassword.error = "Enter your email"
+            }
+            else if (binding.etConfirm.text.toString().equals(binding.etPassword.text.toString()) == false) {
+                binding.etConfirm.error = "Confirm password is not same"
+            }
+            else {
+                System.out.println("Register")
+                auth.signInWithEmailAndPassword(
+                    binding.etEmail.text.toString(), binding.etPassword.text.toString()
+                ).addOnSuccessListener {
+                    Toast.makeText(applicationContext, "Successfully Register", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, FirebaseAuthException::class.java))
+                    finish()
+                }
+                    .addOnFailureListener {
+                        Toast.makeText(applicationContext, "Error${it.toString()}", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+        binding.alreadyRegister.setOnClickListener {
+            var intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
